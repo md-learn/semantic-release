c9ee4af58380de4da2d7f416731b0dbe9458c2b3
Marco Denisi <marcodenisi35@gmail.com>
2020-12-01 21:03:45 +0100

    fix: remove unusued import

--- src/main/java/com/klarna/md/semantic/controller/HelloController.java
+++ src/main/java/com/klarna/md/semantic/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.klarna.md.semantic.controller;
 
 import com.klarna.md.semantic.response.Greeting;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +11,7 @@ public class HelloController {
 
     @GetMapping
     public Greeting hello() {
+
         return new Greeting(1L, "Hello world");
     }
 }
